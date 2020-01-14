48e093af373c8b3952ac1519a9909f53852aef94
Bhavitavaya Prakash Shrivastava <42152956+bhavitavayashrivastava@users.noreply.github.com>
2020-01-15 00:02:16 +0530

    Update Airports.py

--- Airports.py
+++ Airports.py
@@ -1,6 +1,21 @@
 '''
-To find out Airpors in Usa and filter out from the dataset using Pyspark 
-dataset used "airports.text"
+
+
+    Create a Spark program to read the airport data from in/airports.text, find all the airports which are located in United States
+    and output the airport's name and the city's name to out/airports_in_usa.text.
+    Each row of the input file contains the following columns:
+    
+    
+    
+    Airport ID, Name of airport, Main city served by airport, Country where airport is located, IATA/FAA code,
+    ICAO Code, Latitude, Longitude, Altitude, Timezone, DST, Timezone in Olson format
+    Sample output:
+    "Putnam County Airport", "Greencastle"
+    "Dowagiac Municipal Airport", "Dowagiac"
+    
+
+
+
 '''
 
 from pyspark import SparkConf, SparkContext
@@ -21,4 +36,4 @@ airportsFiltered = airports.filter(lambda port: clean_data(port).split(",")[3]==
 
 airportsMap = airportsFiltered.map(lambda port: (clean_data(port).split(",")[1],clean_data(port).split(",")[2]))
 
-airportsMap.saveAsTextFile("airportsName")
+airportsMap.saveAsTextFile("airportsNameAndCity")
